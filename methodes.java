d849b5824d0360b0a2a9ed0f754085d25d63cc81
fionabgz <96353079+fionabgz@users.noreply.github.com>
2022-12-08 15:27:16 +0100

    Create methodes.java

---
+++ methodes.java
@@ -0,0 +1,40 @@
+public class Methodes {
+
+
+    public static int indiceLettreEnInt(char c){
+        int nb=0;
+        switch(c){
+            case 'A':
+                nb=1;
+                break;
+            case 'B':
+                nb=2;
+                break;
+            case 'C':
+                nb=3;
+                break;
+            case 'D':
+                nb=4;
+                break;
+            case 'E':
+                nb=5;
+                break;
+            case 'F':
+                nb=6;
+                break;
+            case 'G':
+                nb=7;
+                break;
+            case 'H':
+                nb=8;
+                break;
+        }
+        return nb;
+    }
+
+    public static boolean caseBlanche(String indice){
+
+
+    }
+
+}
