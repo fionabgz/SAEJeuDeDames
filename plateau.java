f72a1d6ed85302fcb94086c86cf233ff0b349c40
czsoup <118196131+czsoup@users.noreply.github.com>
2022-12-13 14:11:14 +0100

    Create plateau.java

--- plateau.java
+++ plateau.java
@@ -5,13 +5,13 @@ public class Damier {
 
     }
     public static char[][] tableauDuPlateau(){
-        char[][] damier=new char[8][8];
+        char[][] damier=new char[8][8]; // pour passer à dim [10][10] > faisable + ajustement l.55 ajouter "IJ"
 
-        //CAS REMPLIR DAMIER AVEC DES POINT SI CASE VIDE
+        //CAS REMPLIR DAMIER AVEC DES espaces SI CASE VIDE et non diagonale
 
        for (int i=0; i<damier.length;i++){
            for (int j=0; j<damier.length;j++)
-               damier[i][j]='.';
+               damier[i][j]=' ';
        }
         // CAS AFFICHAGE CASES EN NOIR ET BLANC AVEC CHARACTERE ASCII
 /*       for (int i=0; i<damier.length;i++) {
@@ -31,11 +31,21 @@ public class Damier {
                     damier[i][j] = 'N';
             }
         }
+        // affichage des cases en diagonale sans pion dessus au demarrrage
+        for (int i =(damier.length-1)/2;i<(damier.length-(damier.length/2))+1;i++){
+            for (int j = 0; j <damier.length; j++) {
+                if ((i%2 == 0 && j%2 != 0 ) || ( j%2==0 && i%2!=0 )) // remplir avec des point sau mileu
+                    damier[i][j] = '.';
+            }
+        }
+
         for (int i= damier.length-1; i>damier.length/2;i--){
             for (int j = damier.length-1; j>=0; j--){
                 if ((i%2 == 0 && j%2 != 0 ) || ( j%2==0 && i%2!=0 ))
                     damier[i][j] = 'B';
             }
+
+
         }return damier;
     }
 
@@ -60,5 +70,4 @@ public class Damier {
 
 
 }
-
 
