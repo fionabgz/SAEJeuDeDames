5bed0e0f630d0764ad5127abff4bb71ce0562ba0
fionabgz <96353079+fionabgz@users.noreply.github.com>
2022-12-13 16:27:23 +0100

    Update joueur blanc.java

--- joueur blanc.java
+++ joueur blanc.java
@@ -1,42 +1,83 @@
+
 import java.util.Scanner;
 public class JoueurBlanc {
-	
-	public static void tour(Scanner sc) {
-		String chaine;
-		int indiceColonne,indiceLigne;
-		char [][] damier;
-		damier = AfficherDamier.tableauDuPlateau();
-	
-		
-		System.out.println("Joueur 1 c'est à votre tour");
-		do {
-		System.out.println("Quelle est la case du pion que vous voulez bouger?");
-		chaine =sc.nextLine();
-		indiceColonne=Methodes.indiceColonne(chaine.charAt(0));
-		indiceLigne=Methodes.indiceLigne(chaine.charAt(1));
-		System.out.println(indiceColonne+","+ indiceLigne);
-		System.out.print(damier[indiceLigne][indiceColonne]);
 
-		}while(!Methodes.saisieCorrecte(chaine)||!Methodes.pionBlancSurCase(damier[indiceLigne][indiceColonne]));
+    public static void tourBlanc(Scanner sc,char[][]damier ) {
+        String chaine;
+        int indiceColonne,indiceLigne;
+        int indLigneArrivee ,indColonneArrivee;
 
-		
-		
-	}
-	 public static void main(String[] args) {
-	      Scanner sc =new Scanner(System.in);
-	      tour(sc);
-	 }
-	 
-	
+        /*String menuTour="C'est à votre tour \n" +
+                "taper 1 pour entrer/modifier les coordonnées de la case du pion que vous voulez changer \n" +
+                "taper 2  pour entrer/modifier les coordonées de la case sur laquelle vous voulez aller" +
+                "taper 3 pour valider ";
+        */
+                System.out.println("Joueur 1 c'est à votre tour");
+                do {
+                    System.out.println("Quelle est la case du pion que vous voulez bouger?");
+                    chaine = sc.nextLine();
+                    indiceColonne = Methodes.indiceColonne(chaine.charAt(0));
+                    indiceLigne = Methodes.indiceLigne(chaine.charAt(1));
+                } while (!Methodes.saisieCorrecte(chaine) || damier[indiceLigne][indiceColonne]!='B');
+
+                do {
+                    System.out.println("Sur quelle case voulez vous aller ?");
+                    chaine = sc.nextLine();
+                    indColonneArrivee = Methodes.indiceColonne(chaine.charAt(0));
+                    indLigneArrivee = Methodes.indiceLigne(chaine.charAt(1));
+                } while (!Methodes.saisieCorrecte(chaine)||!Methodes.deplacementBlancPossible(damier,indiceLigne,indiceColonne,indLigneArrivee,indColonneArrivee));
+                Methodes.deplacementBlanc(damier,indiceLigne,indiceColonne,indLigneArrivee,indColonneArrivee);
+
+    }
+
+    public static void tourNoir(Scanner sc,char[][]damier ) {
+        String chaine;
+        int indiceColonne,indiceLigne;
+        int indLigneArrivee ,indColonneArrivee;
+
+        System.out.println("Joueur 2 c'est à votre tour");
+        do {
+            System.out.println("Quelle est la case du pion que vous voulez bouger?");
+            chaine = sc.nextLine();
+            indiceColonne = Methodes.indiceColonne(chaine.charAt(0));
+            indiceLigne = Methodes.indiceLigne(chaine.charAt(1));
+        } while (!Methodes.saisieCorrecte(chaine) || damier[indiceLigne][indiceColonne]!='N');
+
+        do {
+            System.out.println("Sur quelle case voulez vous aller ?");
+            chaine = sc.nextLine();
+            indColonneArrivee = Methodes.indiceColonne(chaine.charAt(0));
+            indLigneArrivee = Methodes.indiceLigne(chaine.charAt(1));
+        } while (!Methodes.saisieCorrecte(chaine) ||!Methodes.deplacementNoirPossible(damier,indiceLigne,indiceColonne,indLigneArrivee,indColonneArrivee)||damier[indLigneArrivee][indColonneArrivee]!='.');
+        Methodes.deplacementNoir(damier,indiceLigne,indiceColonne,indLigneArrivee,indColonneArrivee);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[][] damier;
+        damier = Damier.tableauDuPlateau();
+        int cpt = 0;
+        do {
+            Damier.afficherPlateau(damier);
+            tourBlanc(sc, damier);
+            Damier.afficherPlateau(damier);
+            tourNoir(sc,damier);
+            Damier.afficherPlateau(damier);
+            cpt++;
+        } while (cpt < 10);
+    }
+
+
 
 }
 
 /* 	A	B	C	D	E	F	G	H
-0	.	N	.	N	.	N	.	N
-1	N	.	N	.	N	.	N	.
-2	.	N	.	N	.	N	.	N
-3	.	.	.	.	.	.	.	.
-4	.	.	.	.	.	.	.	.
-5	B	.	B	.	B	.	B	.
-6	.	B	.	B	.	B	.	B
-7	B	.	B	.	B	.	B	.*/
+0	 	N	 	N	 	N	 	N
+1	N	 	N	 	N	 	N
+2	 	N	 	N	 	N	 	N
+3	.	 	.	 	.	 	.
+4	 	.	 	.	 	.	 	.
+5	B	 	B	 	B	 	B
+6	 	B	 	B	 	B	 	B
+7	B	 	B	 	B	 	B
+*/
