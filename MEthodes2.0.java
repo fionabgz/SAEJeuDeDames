aff3f5b2a99bc2e452661d480e029a6880b2e1e2
fionabgz <96353079+fionabgz@users.noreply.github.com>
2022-12-14 13:27:54 +0100

    Update MEthodes2.0.java

--- MEthodes2.0.java
+++ MEthodes2.0.java
@@ -1,4 +1,64 @@
+public class Methodes {
 
+    /**
+     * @param c
+     * @return l'indice de la colonne converti en int en utilisant le code ascii
+     */
+    public static int indiceColonne(char c) {
+        int nb = c;
+        if (nb >= 72)
+            return 0;
+        else
+            return (nb - 65);
+    }
+
+    /**
+     * @param ligne
+     * @return l'indice de la ligne converti en int
+     */
+    public static int indiceLigne(char ligne) {
+        return Integer.parseInt(String.valueOf(ligne)) ;
+    }
+
+
+    public static boolean caseVide(char c) {
+        return c == '.';
+    }
+
+    public static boolean caseBlanche(char c ){
+        return c == ' ';
+
+    }
+
+    public static void main(String[] args) {
+        char [][] damier=Damier.tableauDuPlateau();
+    }
+
+    public static boolean saisieCorrecte(String chaine) {
+        String lettre = "ABCDEFGH";
+        return (chaine.length() == 2 || lettre.indexOf(chaine.charAt(0)) != -1 || Integer.parseInt(String.valueOf(chaine.charAt(1))) >= 0 || Integer.parseInt(String.valueOf(chaine.charAt(1))) < 8);
+    }
+
+    public static boolean deplacementBlancPossible(char[][] damier, int ligne,int colonne,int ligneArrivee, int colonneArivee){
+        if (ligne-ligneArrivee==1&& (colonne-colonneArivee==1||colonne-colonneArivee==-1))
+            return true;
+        else if (ligne-ligneArrivee==2){
+            if ((colonne-colonneArivee==2)&&damier[ligne-1][colonne-1]=='N')
+                return true;
+            else if ((colonne-colonneArivee==-2)&&damier[ligne+1][colonne+1]=='N')
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+
+    public static void deplacementBlanc(char [][] damier, int ligne,int colonne,int ligneArrivee, int colonneArivee) {
+        if (ligne-ligneArrivee==1&& (colonne-colonneArivee==1||colonne-colonneArivee==-1)) {
+            damier[ligne][colonne] = '.';
+            damier[ligneArrivee][colonneArivee] = 'B';
+        }
         else if (ligne-ligneArrivee==2){
             if ((colonne-colonneArivee==2)&&damier[ligne-1][colonne-1]=='N') {
                 damier[ligne][colonne] = '.';
@@ -11,7 +71,6 @@
                 damier[ligne +1][colonne +1] = '.';
             }
         }
-
     }
     public static boolean deplacementNoirPossible(char[][] damier, int ligne,int colonne,int ligneArrivee, int colonneArivee) {
         if (ligne - ligneArrivee == -1 && (colonne - colonneArivee == 1 || colonne - colonneArivee == -1)) {
